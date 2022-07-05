ce44d902c83d49548f6a258885bde49142e50f66
OksanaKukalets <80641785+OksanaKukalets@users.noreply.github.com>
2022-07-05 20:46:20 +0300

    Kukalets: modifying JS for regform

--- js/hire_us_contact_form.js
+++ js/hire_us_contact_form.js
@@ -2,59 +2,49 @@ const userName = document.querySelector(".username");
 const phoneNumber = document.querySelector(".phone__number");
 const email = document.querySelector(".email");
 
-function showError(input, message) {
-    const formControl = input.parentElement;
-    formControl.className = 'form-control error';
-    const small = formControl.querySelector('small');
-    small.innerText = message;
-}
+
 function showSuccess(input) {
     const formControl = input.parentElement;
     formControl.className = 'form-control success';
 }
-function checkEmail(input) {
+function showError(input) {
+    const formControl = input.parentElement;
+    formControl.className = 'form-control error';
+}
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+      showError(input);
+    } else if (input.value.length > max) {
+      showError(input);
+    } else {
+      showSuccess(input);
+    }
+  }
+  function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
       showSuccess(input);
     } else {
-      showError(input, 'Email is not valid');
+      showError(input);
     }
   }
   function checkRequired(inputArr) {
     let isRequired = false;
     inputArr.forEach(function(input) {
       if (input.value.trim() === '') {
-        showError(input, `${getFieldName(input)} is required`);
+        showError(input);
         isRequired = true;
       } else {
         showSuccess(input);
       }
     });
-      return isRequired;
+    return isRequired;
   }
-  function checkLength(input, min, max) {
-    if (input.value.length < min) {
-      showError(
-        input,
-        `${getFieldName(input)} must be at least ${min} characters`
-      );
-    } else if (input.value.length > max) {
-      showError(
-        input,
-        `${getFieldName(input)} must be less than ${max} characters`
-      );
-    } else {
-      showSuccess(input);
-    }
-  }
-  form.addEventListener('submit', function(e) {
+form.addEventListener('submit', function(e) {
     e.preventDefault();
-      if(checkRequired([userName, phoneNumber, email])){
+     if (checkRequired([userName, phoneNumber, email])) {
       checkLength(userName, 3, 15);
       checkLength(phoneNumber, 6, 10);
       checkEmail(email);
     }
-    });
-    function getFieldName(input) {
-        return input.id.charAt(0).toUpperCase() + input.id.slice(1);
-    }
+});
