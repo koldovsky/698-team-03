3275594703a3f4452eb08ec9abb8be7cdc554753
OksanaKukalets <80641785+OksanaKukalets@users.noreply.github.com>
2022-07-05 21:33:11 +0300

    Kukalets: js form updating functions

--- js/hire_us_contact_form.js
+++ js/hire_us_contact_form.js
@@ -4,39 +4,39 @@ const phoneNumber = document.querySelector(".phone__number");
 const email = document.querySelector(".email");
 
 
-function showSuccess(input) {
+function showSuccess() {
     const formControl = input.parentElement;
     formControl.className = 'form__control success';
 }
-function showError(input) {
+function showError() {
     const formControl = input.parentElement;
     formControl.className = 'form__control error';
 }
 function checkLength(input, min, max) {
     if (input.value.length < min) {
-      showError(input);
+      showError();
     } else if (input.value.length > max) {
-      showError(input);
+      showError();
     } else {
-      showSuccess(input);
+      showSuccess();
     }
   }
   function checkEmail(input) {
     const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if (re.test(input.value.trim())) {
-      showSuccess(input);
+      showSuccess();
     } else {
-      showError(input);
+      showError();
     }
   }
   function checkRequired(inputArr) {
     let isRequired = false;
     inputArr.forEach(function(input) {
       if (input.value.trim() === '') {
-        showError(input);
+        showError();
         isRequired = true;
       } else {
-        showSuccess(input);
+        showSuccess();
       }
     });
     return isRequired;
