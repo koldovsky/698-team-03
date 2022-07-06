885c829f0b1d13809cfc55f8f87a22df547f4051
Uliana Kolodii <ulianakolodii@gmail.com>
2022-07-06 10:44:58 +0300

    kolodii: upgrade contacts page style

--- js/header__burger-menu.js
+++ js/header__burger-menu.js
@@ -1,21 +1,21 @@
 (function () {
-    const iconBurger = document.querySelector(".burger__menu");
-    const navigationsBody = document.querySelector(".header-navigation__wrapper");
-    iconBurger.addEventListener("click", function (e) {
-      document.body.classList.toggle("lock");
-      iconBurger.classList.toggle("active");
-      navigationsBody.classList.toggle("active");
-    });
-  
-    const navLinks = document.querySelectorAll(".menu-link__mobile");
-    navLinks.forEach((navLink) => {
-      navLink.addEventListener("click", (_) => {
-            if (iconBurger.classList.contains("active")) {
-                document.body.classList.remove("lock");
-                iconBurger.classList.remove("active");
-                navigationsBody.classList.remove("active");
-            }
-          }
-      );
-    });
-  })();
+  const iconBurger = document.querySelector(".burger__menu");
+  const navigationsBody = document.querySelector(".header-navigation__wrapper");
+  iconBurger.addEventListener("click", function (e) {
+    document.body.classList.toggle("lock");
+    iconBurger.classList.toggle("active");
+    navigationsBody.classList.toggle("active");
+  });
+
+  const navLinks = document.querySelectorAll(".menu-link__mobile");
+  navLinks.forEach((navLink) => {
+    navLink.addEventListener("click", (_) => {
+      if (iconBurger.classList.contains("active")) {
+        document.body.classList.remove("lock");
+        iconBurger.classList.remove("active");
+        navigationsBody.classList.remove("active");
+      }
+    }
+    );
+  });
+})();
