122ad75a5df3b1b00194d672a732367259f3d2d6
OksanaKukalets <80641785+OksanaKukalets@users.noreply.github.com>
2022-07-07 20:54:33 +0300

    Kukalets: check required

--- js/hire_us_contact_form.js
+++ js/hire_us_contact_form.js
@@ -40,15 +40,16 @@ function checkLength(input, min, max) {
     });
  }
 form.addEventListener('submit', function(e) {
-    // e.preventDefault();
-     checkRequired([userName, phoneNumber, email]) 
+    e.preventDefault();
+     if (checkRequired([userName, phoneNumber, email])) {
       checkLength(userName, 3, 15);
       checkLength(phoneNumber, 6, 10);
       checkEmail(email);
-     } 
+     }
+    } 
     );  
- /* function submitForm() {
+ function submitForm() {
     if (checkLength(userName, 3, 15) )
-  }  */
+  }  
   
   }) ();
