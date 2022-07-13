6bbc42efcc62580ec7a04e2e4208c36fdcb8a589
Rrinna <irinakukhtyk@gmail.com>
2022-07-13 18:02:37 +0000

    chanche js form

---
+++ js/our-franchisee_contact-form.js
@@ -0,0 +1,8 @@
+(function() {
+    const frForm = document.forms.ourFranchisee;
+    const inputName = frForm.inputName;
+    const inputPhone = frForm.inputPhone;
+    const inputEmail = frForm.inputEmail;
+
+    console.log(inputName.value);
+})();
